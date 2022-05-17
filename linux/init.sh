26322a72a173b98241cf0f90d7af9682cb9fca7c
sword4869 <1084662708@qq.com>
2022-05-17 09:50:11 +0800

    >>

--- linux/init.sh
+++ linux/init.sh
@@ -7,10 +7,10 @@
 # if you want more mirrors, go to https://mirrors.alpinelinux.org/
 
 echo "deb http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
-echo "deb-src http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
+echo "deb-src http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
 
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
-echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 
 # if you don't update & upgrade, then there will be a error, "Error relocating /usr/bin/ssh: __localtime64_r: symbol not found".	
 apt update
